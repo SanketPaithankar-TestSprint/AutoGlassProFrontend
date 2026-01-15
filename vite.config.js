81d2c78796dcdb3bb9b2f5ea5162867b56e2dbd9
SanketPaithankar-TestSprint <sanket.paithankar@testsprint360.com>
2026-01-15 22:16:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   
+  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom', 'zustand', 'axios'],
+          ui: ['antd', '@ant-design/icons'],
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+          pdf: ['jspdf', 'jspdf-autotable'],
+        },
+      },
+    },
+  },
 })
